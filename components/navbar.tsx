c162f7201a9b5eca52e3f80eff2aa7dc5b7fdfb1
yoanastamenova <yoana.stamenovaa@gmail.com>
2025-09-30 16:40:20 +0200

    fix: spacing in navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -6,13 +6,14 @@ export default function Navbar() {
     <nav className="sticky top-0 z-50 bg-amber-50 shadow">
         <div className="container mx-auto flex items-center justify-between px-4 py-4"> 
             <Link href="/" className="hover:text-blue-600"> 
-            <Image src="/logo.png" height={80} width={80} alt="logo"/>
+                <Image src="/logo.png" height={80} width={80} alt="logo"/>
             </Link>
-        <div className="hidden md:flex space-x-9">
-            <Link href="/locations" className="hover:text-blue-600"> Locations</Link>
-            <Link href="/warnings" className="hover:text-blue-600"> Warnings </Link>
-            <Link href="/maps" className="hover:text-blue-600"> Maps </Link>
-        </div>
+            <div className="hidden md:flex gap-10">
+                <Link href="/locations" className="hover:text-blue-600 px-6 py-2"> Locations</Link>
+                <Link href="/warnings" className="hover:text-blue-600 px-6 py-2"> Warnings </Link>
+                <Link href="/maps" className="hover:text-blue-600 px-6 py-2"> Maps </Link>
+                <Link href="/maps" className="hover:text-blue-600 px-6 py-2"> Contact us </Link>
+            </div>
         </div>
     </nav>
   )
