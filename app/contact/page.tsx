8ecb4cca641251feb5b4debef07448b94a515a00
yoanastamenova <yoana.stamenovaa@gmail.com>
2025-10-16 12:54:20 +0200

    feat: start contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,45 @@
-import React from 'react'
+import React from "react";
 
 export default function ContactPage() {
   return (
-    <div> Contact us page</div>
-  )
+    <section className="py-32">
+      <div className="container">
+        <div className="mx-auto flex max-w-7xl flex-col justify-between gap-10 lg:flex-row lg:gap-20">
+
+        {/* Contact Info */}
+        <div className="mx-auto flex max-w-sm flex-col justify-between gap-10">
+          <div className="text-center lg:text-left">
+          <h1 className="mb-2 text-5xl font-semibold lg:mb-1 lg:text-6xl">Contact Us</h1>
+          <p className="text-muted-foreground">
+            We are available for questions, feedback, or collaboration
+            opportunities. Let us know how we can help!
+          </p>
+          </div>
+        </div>
+        <div className="mx-auto w-fit lg:mx-0">
+          <h3 className="mb-6 text-center text-2xl font-semibold lg:text-left"> Contact Details</h3>
+          <ul className="ml-4 list-disc">
+            <li>
+              <span className="font-bold">Phone: </span>
+              "(123) 34567890"
+              </li>
+            <li>
+              <span className="font-bold">Email: </span>
+              <a href="mailto:email@example.com" className="underline">email@example.com</a>
+            </li>
+            <li>
+              <span className="font-bold">Address: </span>
+              Whats the Weather SML
+              Madison Av 5, Building 3, Floor 4
+              46012, Valencia, Spain
+              </li>
+          </ul>
+        </div>
+
+            {/* Contact Form to Fill */}
+            <div className="container mx-auto px-4 py-16 mt-12 relative z-10 p-6"></div>
+            </div>
+        </div>
+    </section>
+  );
 }
