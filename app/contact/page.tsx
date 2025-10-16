02bbc79f483606d4d817fd234aa6cc36537e7aff
yoanastamenova <yoana.stamenovaa@gmail.com>
2025-10-16 14:33:04 +0200

    feat: add form and relevant elements from Shadcn

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,45 +1,183 @@
-import React from "react";
+"use client";
+
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
+import { toast } from "sonner";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const formSchema = z.object({
+  name_9955127597: z.string().min(1),
+  name_9377940356: z.string().min(1),
+  name_7761390004: z.string().email(),
+  name_0647194264: z.string().optional(),
+  name_3259739389: z.string().min(1).max(3000),
+});
 
 export default function ContactPage() {
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+  })
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    try {
+      console.log(values);
+      toast(
+        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+          <code className="text-white">{JSON.stringify(values, null, 2)}</code>
+        </pre>
+      );
+    } catch (error) {
+      console.error("Form submission error", error);
+      toast.error("Failed to submit the form. Please try again.");
+    }
+  }
+
   return (
     <section className="py-32">
       <div className="container">
         <div className="mx-auto flex max-w-7xl flex-col justify-between gap-10 lg:flex-row lg:gap-20">
-
-        {/* Contact Info */}
-        <div className="mx-auto flex max-w-sm flex-col justify-between gap-10">
-          <div className="text-center lg:text-left">
-          <h1 className="mb-2 text-5xl font-semibold lg:mb-1 lg:text-6xl">Contact Us</h1>
-          <p className="text-muted-foreground mt-8">
-            We are available for questions, feedback, or collaboration
-            opportunities. Let us know how we can help!
-          </p>
-          </div>
-        <div className="mx-auto w-fit my-12 lg:mx-0">
-          <h3 className="mb-6 text-center text-2xl font-semibold lg:text-left"> Contact Details</h3>
-          <ul className="ml-4 list-disc">
-            <li>
-              <span className="font-bold">Phone: </span>
-              "(123) 34567890"
-              </li>
-            <li>
-              <span className="font-bold">Email: </span>
-              <a href="mailto:email@example.com" className="underline">email@example.com</a>
-            </li>
-            <li>
-              <span className="font-bold">Address: </span>
-              Whats the Weather SML
-              Madison Av 5, Building 3, Floor 4
-              46012, Valencia, Spain
-              </li>
-          </ul>
-        </div>
-                </div>
-
-            {/* Contact Form to Fill */}
-            <div className="container mx-auto px-4 py-16 mt-12 relative z-10 p-6"></div>
+          {/* Contact Info */}
+          <div className="mx-auto flex max-w-sm flex-col justify-between gap-10">
+            <div className="text-center lg:text-left">
+              <h1 className="mb-2 text-5xl font-semibold lg:mb-1 lg:text-6xl">
+                Contact Us
+              </h1>
+              <p className="text-muted-foreground mt-8">
+                We are available for questions, feedback, or collaboration
+                opportunities. Let us know how we can help!
+              </p>
             </div>
+            <div className="mx-auto w-fit my-12 lg:mx-0">
+              <h3 className="mb-6 text-center text-2xl font-semibold lg:text-left">
+                {" "}
+                Contact Details
+              </h3>
+              <ul className="ml-4 list-disc">
+                <li>
+                  <span className="font-bold">Phone: </span>
+                  "(123) 34567890"
+                </li>
+                <li>
+                  <span className="font-bold">Email: </span>
+                  <a href="mailto:email@example.com" className="underline">
+                    email@example.com
+                  </a>
+                </li>
+                <li>
+                  <span className="font-bold">Address: </span>
+                  Whats the Weather SML Madison Av 5, Building 3, Floor 4 46012,
+                  Valencia, Spain
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          {/* Contact Form to Fill */}
+          <div className="mx-auto flex max-w-3xl flex-col gap-6 rounded-lg border p-10">
+            <Form {...form}>
+              <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="space-y-8 max-w-3xl mx-auto py-10"
+              >
+                <FormField
+                  control={form.control}
+                  name="name_9955127597"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>First Name</FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="First Name"
+                          type="text"
+                          {...field}
+                        />
+                      </FormControl>
+
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="name_9377940356"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Last Name</FormLabel>
+                      <FormControl>
+                        <Input placeholder="Last Name" type="text" {...field} />
+                      </FormControl>
+
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="name_7761390004"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Email</FormLabel>
+                      <FormControl>
+                        <Input placeholder="Email" type="email" {...field} />
+                      </FormControl>
+
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="name_0647194264"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Subject</FormLabel>
+                      <FormControl>
+                        <Input placeholder="Subject" type="text" {...field} />
+                      </FormControl>
+
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="name_3259739389"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Message</FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="Type your message here."
+                          type="text"
+                          {...field}
+                        />
+                      </FormControl>
+
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <Button type="submit">Submit</Button>
+              </form>
+            </Form>
+          </div>
         </div>
+      </div>
     </section>
   );
 }
